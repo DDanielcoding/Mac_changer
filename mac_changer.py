97efc42593fe45255f744c01eb3c8954259ab855
DDanielCoding <draskoczi.daniel@gmail.com>
2024-03-23 12:56:55 -0400

    Update: Variables Based on Command-line Arguments

--- mac_changer.py
+++ mac_changer.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
-interface = input("interface > ")
-new_mac = input("new MAC > ")
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+
+(options, arguments) = parser.parse_args()
+
+interface = options.interface
+new_mac = options.new_mac
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True) # Can change the MAC address of eth0 to the given MAC address!
-subprocess.call("ifconfig " + interface + " up", shell=True)
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
