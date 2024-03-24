8bc5043c750aacd2431686d0860537eaf2251fa8
DDanielCoding <draskoczi.daniel@gmail.com>
2024-03-24 04:55:51 -0400

    Update: Added If statements

--- mac_changer.py
+++ mac_changer.py
@@ -8,7 +8,12 @@ def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
-    return parser.parse_args()
+    (options, arguments) = parser.parse_args()
+    if not options.interface: # it checks if options.interface does not hold a value
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new mac_address, use --help for more info.")
+    return options
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
@@ -17,5 +22,5 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
-(options, arguments) = get_arguments()
+options = get_arguments()
 change_mac(options.interface, options.new_mac)
