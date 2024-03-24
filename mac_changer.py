5e21d99e902b8b343fe96f751088f2d7c3de64bc
DDanielCoding <draskoczi.daniel@gmail.com>
2024-03-24 05:42:27 -0400

    Update: Extracted a Substring Using Regex

--- mac_changer.py
+++ mac_changer.py
@@ -2,7 +2,7 @@
 
 import subprocess
 import optparse
-
+import re
 
 def get_arguments():
     parser = optparse.OptionParser()
@@ -27,3 +27,9 @@ change_mac(options.interface, options.new_mac)
 
 ifconfig_result = subprocess.check_output(["ifconfig", options.interface], text=True)
 print(ifconfig_result)
+
+mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+if mac_address_search_result:
+    print(mac_address_search_result.group(0))
+else:
+    print("[-] Could not read MAC address.")
